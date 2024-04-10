2417559d6de41ccb8219c77e4061bc4f3d957358
Lucas Montecino <lucasmontecino95@gmail.com>
2024-04-10 18:47:27 -0300

    error handling server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const blogRouter = require("./routes/blog-route");
 require("./db");
 
@@ -7,11 +8,17 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("combined"));
 
 app.use("/api", blogRouter);
 
 app.use("/api", (req, res) => {
   res.json("Hello World");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
 });
 
 app.listen(3000, () => console.log(`Server listening on port 3000...`));
