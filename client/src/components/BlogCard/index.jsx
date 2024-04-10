a15855cefdfbb978dc2eb2d06f5b15dd5ec5c8fb
Lucas Montecino <lucasmontecino95@gmail.com>
2024-04-10 17:23:12 -0300

    creating solution for description blogs

--- client/src/components/BlogCard/index.jsx
+++ client/src/components/BlogCard/index.jsx
@@ -8,11 +8,13 @@ export default function BlogCard({ blog, deleteBlog }) {
       key={blog._id}
       className="rounded-md border bg-white shadow-md flex flex-col justify-between px-4 py-4 w-72 h-56"
     >
-      <p className="border border-amber-300 bg-amber-100 shadow-md rounded-md px-3 py-1">
+      <p className="border border-amber-300 bg-amber-100 shadow-md rounded-md px-3 py-2 min-h-24 max-h-32 line-clamp-4">
         {blog.description}
       </p>
       <div className="flex items-center justify-between">
-        <h2 className="text-left font-bold text-teal-700">{blog.title}</h2>
+        <h2 className="text-left font-bold text-teal-700 max-w-48 truncate">
+          {blog.title}
+        </h2>
         <div className="flex gap-2">
           <NavLink to={`/${blog._id}`}>
             <AiOutlineEdit className="text-teal-700 text-xl cursor-pointer" />
