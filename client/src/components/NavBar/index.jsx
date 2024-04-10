584e1f10d4549f7dbb2efbc0d1739888cf6321b2
Lucas Montecino <lucasmontecino95@gmail.com>
2024-04-10 10:25:53 -0300

    changes in App and create the navbar component

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,3 +1,21 @@
+import { NavLink } from "react-router-dom";
+
 export default function NavBar() {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <nav>
+        <h1>Blog Website</h1>
+        <ul>
+          <li>
+            <NavLink to={"/"} className={"text-xl text-red-400"}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/addBlog"}>Add Blog</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
